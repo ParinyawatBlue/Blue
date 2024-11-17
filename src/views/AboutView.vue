cd120ae2887f6b13d581c252437d4a57637a0692
unknown <67703590@chonburi.spu.ac.th>
2024-11-17 11:58:35 +0700

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <h1>This is an about page</h1>
+      <p>Parinyawat Kumseeya</p>
+      <button type="button" class="btn btn-dark">Dark</button>
+      &nbsp;
+      <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
   </div>
 </template>
